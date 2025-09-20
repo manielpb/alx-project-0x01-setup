394ee74432b7f2dfa5e1b39ce83c685229ec1555
maniel <manuelpepr99@icloud.com>
2025-09-20 23:43:39 +0000

    update

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -3,7 +3,8 @@ import { UserProps } from "@/interfaces";
 const UserCard: React.FC<UserProps> =({id, name,  username, email, address, phone, website, company }) => {
     return (
         <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-            
+
         </div>
     )
 }
+
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,10 @@
-import React from "react";
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
