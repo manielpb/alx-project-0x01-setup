8ccd493f828d3b29d2ebd9b94573d4bcdb051cc0
maniel <manuelpepr99@icloud.com>
2025-09-20 23:40:06 +0000

    update

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,6 +1,32 @@
+import { StringLiteral } from "typescript";
+
 export interface PostProps {
     userId: number;
     id: number;
     title: string;
     body: string;
+}
+
+export interface UserProps {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: number;
+        geo: {
+            lat: number;
+            lng: number;
+        }
+    };
+    phone: number;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    }
 }
