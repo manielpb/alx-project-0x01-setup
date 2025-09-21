7a264b99274dd05889aed1b2643b4b584bd258b2
maniel <manuelpepr99@icloud.com>
2025-09-21 13:06:38 +0000

    update

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -43,3 +43,27 @@ export  interface PostModalProps{
     onSubmit: (post: PostData) => void;
 
 }
+
+export interface UserData {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: number;
+        geo: {
+            lat: number;
+            lng: number;
+        }
+    };
+    phone: number;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    }
+}
